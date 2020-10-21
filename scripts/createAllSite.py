324d585c6090682b08a6be066a87ced64f3423fc
fakhrip <fakhrip@users.noreply.github.com>
2020-10-21 03:53:08 +0000

    deploy: e106cf9e22a683f71a4e0901aa2c1be326f810d8

--- scripts/createAllSite.py
+++ scripts/createAllSite.py
@@ -15,6 +15,12 @@ BLOGTEMPLATE = """<!DOCTYPE html>
     <link href="https://fonts.googleapis.com/css?family=Fira+Code|Press+Start+2P|Bitter:wght@700&&family=Raleway&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/base.css">
     <link rel="stylesheet" href="/native.css">
+    <link rel="apple-touch-icon" href="../favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="512x512"  href="../favicon/android-chrome-512x512.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="../favicon/android-chrome-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../favicon/favicon-16x16.png">
+    <link rel="manifest" href="../favicon/site.webmanifest">
 
     <title>f4r4w4y</title>
 </head>
