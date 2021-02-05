b05124f0a038d4e1dae6f13d7615c727d2222f6a
fakhrip <fakhrip@users.noreply.github.com>
2021-02-05 13:52:20 +0000

    deploy: b0e1d10870158c616597e2c4d4730858fe115d53

--- scripts/createAllSite.py
+++ scripts/createAllSite.py
@@ -12,7 +12,7 @@ BLOGTEMPLATE = """<!DOCTYPE html>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    /* OGP (https://ogp.me/) */
+    <!-- OGP (https://ogp.me/) -->
     <meta property="og:site_name" content="f4r4w4y"/>
     <meta property="og:description" content="{{ desc }}"/>
     <meta property="og:title" content="{{ title }}"/>
