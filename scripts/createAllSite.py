690064fb3997f0ca839d60f80c766dce5238367e
fakhrip <fakhriputra123s@gmail.com>
2021-07-30 21:22:27 +0700

    refactor script and fix datetime bug

--- scripts/createAllSite.py
+++ scripts/createAllSite.py
@@ -7,6 +7,7 @@ import markdown2, os, json
 
 ARTICLE_TEMPLATE = open("./templates/articleTemplate.html", "r").read()
 
+
 def main():
     print("[+] Running all sites creation sequence")
     allSites = [f for f in os.listdir("./sites") if isfile(join("./sites", f))]
@@ -15,100 +16,103 @@ def main():
     print("[+] Making sure /blogs exist")
     if not os.path.exists("./blogs"):
         os.mkdir("./blogs")
-        print("[|] Directory " , "/blogs" ,  " Created ")
-    else:    
-        print("[|] Directory " , "/blogs" ,  " already exists")
-    
+        print("[|] Directory ", "/blogs", " Created ")
+    else:
+        print("[|] Directory ", "/blogs", " already exists")
+
     print("[+] ===")
     print("[+] Converting /sites to /blogs")
-    for site in allSites :
+    for site in allSites:
         print("[|] Working on '{}'".format(site))
-        siteFile = open("./sites/" + site, "r")
-        blogFile = open("./blogs/" + site.replace(".md","") + ".html", "w+")
-        
-        siteContent = siteFile.read()
+
+        siteContent = open("./sites/" + site, "r").read()
         siteMD = siteContent.split("--+--+--+--\n")[1]
         siteMeta = siteContent.split("--+--+--+--\n")[0]
-        convertedSite = markdown2.markdown(siteMD, extras = [
-            "footnotes", 
-            "fenced-code-blocks", 
-            "tables", 
-            "markdown-in-html",
-            "target-blank-links"
-        ])
-
-        siteFile.close()
-
-        for meta in siteMeta.split("\n"):
-            if "Tags" == meta.split(": ")[0] :
-                tags = meta.split(": ")[1].split("|")
-            if "Times" == meta.split(": ")[0] :
-                times = markdown2.markdown(meta.split(": ")[1], extras = [
-                    "footnotes", 
-                    "fenced-code-blocks", 
-                    "tables", 
-                    "markdown-in-html",
-                    "target-blank-links"
-                ])
-            if "TLDR" == meta.split(": ")[0] :
-                desc = meta.split(": ")[1]
-                tldr = markdown2.markdown(meta.split(": ")[1], extras = [
-                    "footnotes", 
-                    "fenced-code-blocks", 
-                    "tables", 
-                    "markdown-in-html",
-                    "target-blank-links"
-                ])
-
-        renderedResult = Template(ARTICLE_TEMPLATE).render(
-            contents = convertedSite,
-            updatedDate = datetime.fromtimestamp(blogLastUpdated, tz=timezone('Asia/Jakarta')).strftime("%B %d, %Y"),
-            updatedTime = datetime.fromtimestamp(blogLastUpdated, tz=timezone('Asia/Jakarta')).strftime("%H:%M:%S"),
-            tags = tags,
-            times = times,
-            tldr = tldr,
-            site = site,
-            desc = desc,
-            title = site.replace('-',' ').replace('.md','')
+        convertedSite = markdown2.markdown(
+            siteMD,
+            extras=[
+                "footnotes",
+                "fenced-code-blocks",
+                "tables",
+                "markdown-in-html",
+                "target-blank-links",
+            ],
         )
 
-        blogFile.write(renderedResult)
-        blogFile.close()
+        for meta in siteMeta.split("\n"):
+            if "Tags" == meta.split(": ")[0]:
+                tags = meta.split(": ")[1].split("|")
+            if "Times" == meta.split(": ")[0]:
+                times = markdown2.markdown(
+                    meta.split(": ")[1],
+                    extras=[
+                        "footnotes",
+                        "fenced-code-blocks",
+                        "tables",
+                        "markdown-in-html",
+                        "target-blank-links",
+                    ],
+                )
+            if "TLDR" == meta.split(": ")[0]:
+                desc = meta.split(": ")[1]
+                tldr = markdown2.markdown(
+                    meta.split(": ")[1],
+                    extras=[
+                        "footnotes",
+                        "fenced-code-blocks",
+                        "tables",
+                        "markdown-in-html",
+                        "target-blank-links",
+                    ],
+                )
+
+        datetime = (
+            os.popen(f"echo $(git log -1 --format=%cd ./sites/{site})")
+            .read()
+            .strip()
+            .split()
+        )
+
+        renderedResult = Template(ARTICLE_TEMPLATE).render(
+            contents=convertedSite,
+            updatedDate=f"{datetime[1]} {datetime[2]}, {datetime[4]}",
+            updatedTime=f"{datetime[3]}",
+            tags=tags,
+            times=times,
+            tldr=tldr,
+            site=site,
+            desc=desc,
+            title=site.replace("-", " ").replace(".md", ""),
+        )
+
+        open("./blogs/" + site.replace(".md", "") + ".html", "w+").write(renderedResult)
 
     print("[+] ===")
     print("[+] Injecting all sites to blogs.html")
     allBlogs = [f for f in os.listdir("./blogs") if isfile(join("./blogs", f))]
     allBlogs = sorted(allBlogs)
-    
-    with open("./templates/blogsTemplate.html", "r") as blogsHTMLFile :
-        blogsHTMLTemplate = blogsHTMLFile.read()
-    
-        renderedResult = Template(blogsHTMLTemplate).render(sites = allBlogs)
-        
-        blogsFile = open("./blogs.html", "w+")
-        blogsFile.write(renderedResult)
-        blogsFile.close()
+
+    blogsHTMLTemplate = open("./templates/blogsTemplate.html", "r").read()
+    open("./blogs.html", "w+").write(Template(blogsHTMLTemplate).render(sites=allBlogs))
 
     print("[+] Updating date and time in index.html")
-    cur_date = datetime.now(timezone('Asia/Jakarta')).strftime("%B %d, %Y")
-    cur_time = datetime.now(timezone('Asia/Jakarta')).strftime("%H:%M:%S")
+    cur_date = datetime.now(timezone("Asia/Jakarta")).strftime("%B %d, %Y")
+    cur_time = datetime.now(timezone("Asia/Jakarta")).strftime("%H:%M:%S")
 
-    github_urls = check_output('curl "https://api.github.com/users/fakhrip/repos?per_page=100" | jq "[ .[] | select(.archived == \"false\") | .html_url ]"', shell=True)
+    github_urls = check_output(
+        'curl "https://api.github.com/users/fakhrip/repos?per_page=100" | jq "[ .[] | select(.archived == "false") | .html_url ]"',
+        shell=True,
+    )
     github_urls = [url for url in json.loads(github_urls)]
     github_urls = sorted(github_urls)
-    
-    with open("./templates/indexTemplate.html", "r") as indexHTMLFile :
-        indexHTMLTemplate = indexHTMLFile.read()
 
-        renderedResult = Template(indexHTMLTemplate).render(
-            github_urls = github_urls,
-            updatedDate = cur_date, 
-            updatedTime = cur_time
+    indexHTMLTemplate = open("./templates/indexTemplate.html", "r").read()
+    open("./index.html", "w+").write(
+        Template(indexHTMLTemplate).render(
+            github_urls=github_urls, updatedDate=cur_date, updatedTime=cur_time
         )
-        
-        indexFile = open("./index.html", "w+")
-        indexFile.write(renderedResult)
-        indexFile.close()
+    )
 
-if __name__ == "__main__" :
+
+if __name__ == "__main__":
     main()
